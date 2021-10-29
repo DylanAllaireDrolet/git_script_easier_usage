721eb7a11237090cefcf0a794bd1485a60f445be
6188793 <dylan.allaire.drolet.13@gmail.com>
2021-10-29 15:55:32 -0400

    This is using Batch to commit

---
+++ gitScript.bat
@@ -0,0 +1,41 @@
+@echo off
+
+echo Put this file in the source of your GIT repository
+git status
+pause
+:start
+cls
+echo If you want to pull press 1
+echo If you want to push all press 2
+echo If you want repository status press 3
+echo If you want to exit press 'Q'
+set /p choicevar=Your choice : 
+if %choicevar%==1 goto pull
+if %choicevar%==2 goto push
+if %choicevar%==3 goto status
+if %choicevar%==q goto exitP
+if %choicevar%==Q goto exitP
+
+
+
+:pull
+git pull
+echo Pulled
+pause
+goto start
+:push
+echo Enter comment for commit
+set /p commentvar=Your comment : 
+
+git add .
+git commit -m "%commentvar%"
+git push
+echo Pushed
+pause
+goto start
+:status
+git status
+pause
+goto start
+:exitP
+exit
